cfb6c343bc554732f66f6cc90ab61cb2e1ee4576
Viscoale97 <lab2atpolito@outlook.it>
2024-11-07 14:28:51 +0100

    Saving to file

--- Assets/Scripts/Chatbot.cs
+++ Assets/Scripts/Chatbot.cs
@@ -8,6 +8,7 @@ using Meta.WitAi.Json;
 using System.Collections.Generic;
 using Meta.WitAi;
 using OpenAI;
+using System.IO;
 
 
 // A struct to help in creating the Json object to be sent to the rasa server
@@ -51,8 +52,12 @@ namespace Meta.WitAi.TTS.Samples
         // Whether voice is activated
         public bool IsActive => _active;
         private bool _active = false;
+
+        string conversation;
+        string myFilePath, filename, filePath;
     
         private OpenAIApi openai = new OpenAIApi();
+        private DateTime startTime;
 
         private List<ChatMessage> messages = new List<ChatMessage>();
         private string prompt = "You are a knowledgeable assistant specializing in intraosseous injections. Provide brief, precise answers within 140 characters. If asked a question unrelated to intraosseous injections, respond with, 'I’m here to help with intraosseous injections! If you’re done, feel free to close this session.' When a user asks to view something, respond with: 'Now you will be able to see' followed by the name of the item or object.";
@@ -74,97 +79,121 @@ namespace Meta.WitAi.TTS.Samples
             npcText.text = "Hey! I'll be your virtual assistant during the Intraosseous Insertion simulation." +
                 " Please enter your name in the window in front of you to start the simulation " +
                 "and feel free to ask me any questions if you have any doubts.";
+           
+            myFilePath = Application.persistentDataPath + "/TestsOpenAI";
+            if (!Directory.Exists(myFilePath))
+            {
+                Debug.Log("Creating new folder");
+                Directory.CreateDirectory(myFilePath);
+            }
+            filename = "TestOpenAI_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
+
+            filePath = Path.Combine(myFilePath, filename);
+
+            conversation = npcText.text;
             StartCoroutine(Wait(3f));
         }
         
         
         private async void SendMessageToRasa()
         {
+            conversation = conversation + "\n user: \n" + inputField.text;
             Debug.Log("In SendMessageToRasa");
             Debug.Log("Input field:" + inputField.text);
             /*if (npcText != null)
             {
                 npcText.text = "message received";  
             }*/
-           
-            var newMessage = new ChatMessage()
+            startTime = DateTime.Now;
+            if (inputField.text == "")
             {
-                Role = "user",
-                Content = inputField.text
-            };
-            
-            Debug.Log("newMessage:" + newMessage);
-            
-
-            if (messages.Count == 0) newMessage.Content = prompt + "\n" + inputField.text; 
-            
-            messages.Add(newMessage);
-            
-            //button.enabled = false;
-            inputField.text = "";
-            inputField.enabled = false;
-            
-            // Complete the instruction
-            var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
-            {
-                Model = "ft:gpt-4o-mini-2024-07-18:personal:io-tenth-experiment:ANtYAzny",
-                Messages = messages
-            });
-
-            if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
-            {
-                var message = completionResponse.Choices[0].Message;
-                message.Content = message.Content.Trim();
-                
-                messages.Add(message);
-                npcText.text = message.Content;
-                _speaker.SpeakQueued(message.Content);
-                
-                
-                string text = message.Content;
-                if (text.Contains("able to see") && text.Contains("access point"))
-                {
-                    SetActiveSuggestion(_insertionPositionSuggestion);
-                }
-                else if (text.Contains("able to see") && (text.Contains("angle")||text.Contains("inclination")))
-                {
-                    SetActiveSuggestion(_drillingInclinationSuggestion);
-                }
-                else if (text.Contains("able to see") && text.Contains("needle") && text.Contains("correct") && text.Contains("insert"))
-                {
-                    Needle needle = _drill.GetComponent<PowerDrill>().GetNeedle();
-                    EnableLine(needle);
-                    Debug.Log("ENTRATO");
-                }
-                else if(text.Contains("able to see") && text.Contains("15mm"))
-                {
-                    EnableSuggestion(_15mmNeedle);
-                }
-                else if(text.Contains("able to see") && text.Contains("25mm"))
-                {
-                    EnableSuggestion(_25mmNeedle);
-                }
-                else if(text.Contains("able to see") && text.Contains("40mm"))
-                {
-                    EnableSuggestion(_40mmNeedle);
-                }
-                else if(text.Contains("able to see") && (text.Contains("Connect") || text.Contains("connect")))
-                {
-                    EnableSuggestion(_connector);
-                }
-                else if(text.Contains("able to see") && (text.Contains("Stabilizer") || text.Contains("stabilize")))
-                {
-                    EnableSuggestion(_stabilizer);
-                }     
-                else if(text.Contains("able to see") && (text.Contains("Power") || text.Contains("power") || text.Contains("drill") || text.Contains("driver")))
-                {
-                    EnableSuggestion(_drill);
-                }
+                npcText.text = "I didn't understand. Could you repeat?";
+                _speaker.SpeakQueued(npcText.text);
             }
             else
             {
-                Debug.LogWarning("No text was generated from this prompt.");
+                var newMessage = new ChatMessage()
+                {
+                    Role = "user",
+                    Content = inputField.text
+                };
+
+                Debug.Log("newMessage:" + newMessage);
+
+
+                if (messages.Count == 0) newMessage.Content = prompt + "\n" + inputField.text;
+
+                messages.Add(newMessage);
+
+                //button.enabled = false;
+                inputField.text = "";
+                inputField.enabled = false;
+
+                // Complete the instruction
+                var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
+                {
+                    Model = "ft:gpt-4o-mini-2024-07-18:personal:io-tenth-experiment:ANtYAzny",
+                    Messages = messages
+                });
+
+                if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
+                {
+                    var message = completionResponse.Choices[0].Message;
+                    message.Content = message.Content.Trim();
+
+                    messages.Add(message);
+                    npcText.text = message.Content;
+                    _speaker.SpeakQueued(npcText.text);
+                    conversation = conversation + "\n assistant: \n" + npcText.text;
+
+
+                    string text = message.Content;
+                    if (text.Contains("able to see") && text.Contains("access point"))
+                    {
+                        SetActiveSuggestion(_insertionPositionSuggestion);
+                    }
+                    else if (text.Contains("able to see") && (text.Contains("angle") || text.Contains("inclination")))
+                    {
+                        SetActiveSuggestion(_drillingInclinationSuggestion);
+                    }
+                    else if (text.Contains("able to see") && text.Contains("needle") && text.Contains("correct") && text.Contains("insert"))
+                    {
+                        Needle needle = _drill.GetComponent<PowerDrill>().GetNeedle();
+                        EnableLine(needle);
+                        Debug.Log("ENTRATO");
+                    }
+                    else if (text.Contains("able to see") && text.Contains("15mm"))
+                    {
+                        EnableSuggestion(_15mmNeedle);
+                    }
+                    else if (text.Contains("able to see") && text.Contains("25mm"))
+                    {
+                        EnableSuggestion(_25mmNeedle);
+                    }
+                    else if (text.Contains("able to see") && text.Contains("40mm"))
+                    {
+                        EnableSuggestion(_40mmNeedle);
+                    }
+                    else if (text.Contains("able to see") && (text.Contains("Connect") || text.Contains("connect")))
+                    {
+                        EnableSuggestion(_connector);
+                    }
+                    else if (text.Contains("able to see") && (text.Contains("Stabilizer") || text.Contains("stabilize")))
+                    {
+                        EnableSuggestion(_stabilizer);
+                    }
+                    else if (text.Contains("able to see") && (text.Contains("Power") || text.Contains("power") || text.Contains("drill") || text.Contains("driver")))
+                    {
+                        EnableSuggestion(_drill);
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning("No text was generated from this prompt.");
+                }
+
             }
+
 
             //button.enabled = true;
             inputField.enabled = true;
@@ -223,6 +252,18 @@ namespace Meta.WitAi.TTS.Samples
         {
             Debug.Log("In SetText");
             inputField.text = text;
+        }
+
+        private IEnumerator LogTimeOnSpeechStart()
+        {
+            while (!_speaker.IsSpeaking)
+            {
+                yield return null;
+            }
+
+            TimeSpan timeElapsed = DateTime.Now - startTime;
+            Debug.Log($"Time elapsed from SendMessageToRasa to actual speech start: {timeElapsed.TotalMilliseconds} ms");
+            conversation = conversation + "\n time : \n" +  timeElapsed.TotalMilliseconds + "ms\n\n";
         }
 
         // Add delegates
@@ -357,6 +398,19 @@ namespace Meta.WitAi.TTS.Samples
         {
             Debug.Log("In Repeat");
             _speaker.Speak(npcText.text);
+            
+        }
+
+        private void Update()
+        {
+            LogTimeOnSpeechStart();
+        }
+
+
+        private void OnApplicationQuit()
+        {
+            File.WriteAllText(filePath, conversation);
+            Debug.Log("saved to file to "+ filePath);
         }
     }
 }
